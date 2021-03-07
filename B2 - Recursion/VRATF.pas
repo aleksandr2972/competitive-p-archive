4d0970e11621aad330b4661aa265b7cfca2992f3
Alexander <k9514753@gmail.com>
2021-03-07 19:30:05 +0700

    Add files via upload

---
+++ B2 - Recursion/VRATF.pas
@@ -0,0 +1,23 @@
+var
+        f:text;
+        n,k: longint;
+
+        function bo(n,k: longint): longint;
+        begin
+                if (n <= k) or ((n-k) mod 2 <> 0) then
+                        exit(1);
+                bo:= bo((n+k) div 2, k) + bo((n-k) div 2,k);
+        end;
+begin
+        assign(f,'bai3.inp');
+        reset(f);
+        read(f,n);
+        read(f,k);
+        close(f);
+
+        assign(f,'bai3.out');
+        rewrite(f);
+        writeln(f,bo(n,k));
+        close(f);
+
+end.
